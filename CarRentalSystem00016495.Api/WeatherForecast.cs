242640f724bba020d1471ca5a7e7a63b993bdaa3
Nozimjon04 <117529725+Nozimjon04@users.noreply.github.com>
2024-12-03 01:31:53 +0500

    Add project files.

---
+++ CarRentalSystem00016495.Api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace CarRentalSystem00016495.Api
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
